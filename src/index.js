ed0feeddb69bbeee7f973c54c159659015a3033e
ara fa adri <arafa.sco@gmail.com>
2024-10-05 16:53:02 +0700

    create index in src

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.listen(5000, ()=>{
+    console.log('server running at port 5000');
+});
