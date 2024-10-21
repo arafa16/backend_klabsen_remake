69822d7303f8cd66a8ea9af91a0d898fcb6f1195
ara fa adri <arafa.sco@gmail.com>
2024-10-21 10:22:10 +0700

    create export perhitungan controller and route

--- src/routes/perhitungan.route.js
+++ src/routes/perhitungan.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const {verifyToken} = require('../middleware/auth.middleware.js');
 const { 
-    getCountDatas
+    getCountDatas,
+    exportCountDatas
 } = require('../controllers/perhitungan.controller.js');
 
 const router = express.Router();
 
 router.get('/count', verifyToken, getCountDatas);
+router.get('/export/count', verifyToken, exportCountDatas);
 
 module.exports = router;
