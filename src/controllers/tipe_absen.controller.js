e1f6d82605c934edebe6cd303841195e30121c2b
ara fa adri <arafa.sco@gmail.com>
2026-04-14 16:45:56 +0700

    build: create absen onsite by application

--- src/controllers/tipe_absen.controller.js
+++ src/controllers/tipe_absen.controller.js
@@ -1,248 +1,238 @@
-const {
-    tipe_absen:tipeAbsenModel,
-} = require('../models/index.js');
-const {Op} = require('sequelize');
+const { tipe_absen: tipeAbsenModel } = require("../models/index.js");
+const { Op } = require("sequelize");
 
-const getDatas = async(req, res) => {
-    const {sort} = req.query;
+const getDatas = async (req, res) => {
+  const { sort } = req.query;
 
-    let sortList = {};
+  let sortList = {};
 
-    if(sort){
-        sortList = sort;
-    }else{
-        sortList ='name';
-    }
+  if (sort) {
+    sortList = sort;
+  } else {
+    sortList = "name";
+  }
 
-    try {
-        const result = await tipeAbsenModel.findAll({
-            order:[sortList]
-        });
-
-        return res.status(200).json({
-            status:200,
-            success:true,
-            datas: {
-                data:result,
-                message: "success"
-            }
-        });
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
-
-const getDataTable = async(req, res) => {
-    const {search, sort} = req.query;
-
-    const queryObject = {};
-    const querySearchObject = {};
-    let sortList = {};
-
-    if(search){
-        querySearchObject.name = {[Op.like]:`%${search}%`}
-    }else{
-        querySearchObject.name = {[Op.like]:`%${''}%`}
-    }
-
-    const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 10;
-    const offset = Number(page - 1) * limit;
-
-    if(sort){
-        sortList = sort;
-    }else{
-        sortList ='name';
-    }
-
-    try {
-        const result = await tipeAbsenModel.findAndCountAll({
-            where:[
-                queryObject,
-                {[Op.or]:querySearchObject}
-            ],
-            limit,
-            offset,
-            order:[sortList]
-        });
-
-        return res.status(200).json({
-            status:200,
-            success:true,
-            datas: {
-                data:result,
-                message: "success"
-            }
-        });
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
-
-const getDataById = async(req, res) => {
-    try {
-        const result = await tipeAbsenModel.findOne({
-            where:{
-                uuid:req.params.id
-            }
-        });
-
-        return res.status(200).json({
-            status:200,
-            success:true,
-            datas: {
-                data:result,
-                message: "success"
-            }
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
-
-const createData = async(req, res) => {
-    const {name, code, is_active, is_select} = req.body;
-
-    try {
-        await tipeAbsenModel.create({
-            name:name,
-            code:code,
-            is_active:is_active,
-            is_select:is_select
-        });
-
-        return res.status(201).json({
-            status:201,
-            success:true,
-            datas: {
-                data:null,
-                message: "success"
-            }
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
-
-const updateData = async(req, res) => {
-    const {name, code, is_active, is_select} = req.body;
-
-    const findData = await tipeAbsenModel.findOne({
-        where:{
-            uuid:req.params.id
-        }
+  try {
+    const result = await tipeAbsenModel.findAll({
+      order: [sortList],
     });
 
-    if(!findData){
-        return res.status(404).json({
-            status:404,
-            success:false,
-            datas: {
-                message: "data isn't found"
-            }
-        });
-    }
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      datas: {
+        data: result,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
 
-    try {
-        await findData.update({
-            name:name,
-            code:code,
-            is_active:is_active,
-            is_select:is_select
-        });
+const getDataTable = async (req, res) => {
+  const { search, sort } = req.query;
 
-        return res.status(201).json({
-            status:201,
-            success:true,
-            datas: {
-                data:null,
-                message: "success"
-            }
-        });
+  const queryObject = {};
+  const querySearchObject = {};
+  let sortList = {};
 
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
+  if (search) {
+    querySearchObject.name = { [Op.like]: `%${search}%` };
+  } else {
+    querySearchObject.name = { [Op.like]: `%${""}%` };
+  }
 
-const deleteData = async(req, res) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const offset = Number(page - 1) * limit;
 
-    const findData = await tipeAbsenModel.findOne({
-        where:{
-            uuid:req.params.id
-        }
+  if (sort) {
+    sortList = sort;
+  } else {
+    sortList = "code";
+  }
+
+  try {
+    const result = await tipeAbsenModel.findAndCountAll({
+      where: [queryObject, { [Op.or]: querySearchObject }],
+      limit,
+      offset,
+      order: [sortList],
     });
 
-    if(!findData){
-        return res.status(404).json({
-            status:404,
-            success:false,
-            datas: {
-                message: "data isn't found"
-            }
-        });
-    }
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      datas: {
+        data: result,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
 
-    try {
-        await findData.destroy();
+const getDataById = async (req, res) => {
+  try {
+    const result = await tipeAbsenModel.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
 
-        return res.status(201).json({
-            status:201,
-            success:true,
-            datas: {
-                data:null,
-                message: "success"
-            }
-        });
+    return res.status(200).json({
+      status: 200,
+      success: true,
+      datas: {
+        data: result,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
 
-    } catch (error) {
-        return res.status(500).json({
-            status:500,
-            success:false,
-            datas: {
-                message: error.message
-            }
-        });
-    }
-}
+const createData = async (req, res) => {
+  const { name, code, is_active, is_select } = req.body;
+
+  try {
+    await tipeAbsenModel.create({
+      name: name,
+      code: code,
+      is_active: is_active,
+      is_select: is_select,
+    });
+
+    return res.status(201).json({
+      status: 201,
+      success: true,
+      datas: {
+        data: null,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
+
+const updateData = async (req, res) => {
+  const { name, code, is_active, is_select } = req.body;
+
+  const findData = await tipeAbsenModel.findOne({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+
+  if (!findData) {
+    return res.status(404).json({
+      status: 404,
+      success: false,
+      datas: {
+        message: "data isn't found",
+      },
+    });
+  }
+
+  try {
+    await findData.update({
+      name: name,
+      code: code,
+      is_active: is_active,
+      is_select: is_select,
+    });
+
+    return res.status(201).json({
+      status: 201,
+      success: true,
+      datas: {
+        data: null,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
+
+const deleteData = async (req, res) => {
+  const findData = await tipeAbsenModel.findOne({
+    where: {
+      uuid: req.params.id,
+    },
+  });
+
+  if (!findData) {
+    return res.status(404).json({
+      status: 404,
+      success: false,
+      datas: {
+        message: "data isn't found",
+      },
+    });
+  }
+
+  try {
+    await findData.destroy();
+
+    return res.status(201).json({
+      status: 201,
+      success: true,
+      datas: {
+        data: null,
+        message: "success",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      success: false,
+      datas: {
+        message: error.message,
+      },
+    });
+  }
+};
 
 module.exports = {
-    getDatas,
-    getDataTable,
-    getDataById,
-    createData,
-    updateData,
-    deleteData
-}
+  getDatas,
+  getDataTable,
+  getDataById,
+  createData,
+  updateData,
+  deleteData,
+};
